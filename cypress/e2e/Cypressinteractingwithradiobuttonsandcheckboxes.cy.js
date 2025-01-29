796baee1071bbf7983e21c969359ec9ab0aaae14
Rashid <rashidmohammed366@gmail.com>
2025-01-29 22:52:25 +0530

    i

---
+++ cypress/e2e/Cypressinteractingwithradiobuttonsandcheckboxes.cy.js
@@ -0,0 +1,29 @@
+// describe("interacting with radiobuttons and checkboxes", () => {
+    // it("radiobuttons", ()=> {
+        // cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        //cy.get("[value = radio1]").click().should("be.checked") // selecting radio buttons
+        // cy.get("[value = radio1]").should('exist') // visibility of radio buttons 
+        // cy.get("[value = radio1]").check().should("be.checked") // selecting radio buttons
+
+
+
+        // it("checkboxes" ,() =>{
+        //     cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+            // cy.get("#checkBoxOption1").should("be.visible")
+            // cy.get("#checkBoxOption1").click()
+            // cy.get("#checkBoxOption1").check().should("be.checked")
+            // cy.get("#checkBoxOption1").uncheck().should("not.be.checked") //unselecting checkboxes
+
+            // //selcting all the checkboxes
+            // cy.get("input[type ='checkbox']").check().should("be.checked") // this is we should use for select all the checkboxes
+            // cy.get("input[type ='checkbox']").uncheck().should("not.be.checked") // this is we should use for unselect all the checkboxes
+        
+            //select first check box
+            // cy.get("input[type ='checkbox']").first().check().should("be.checked")
+            // cy.get("input[type ='checkbox']").last().check().should("be.checked")
+            //  cy.get("input[type='checkbox']").eq(1).check().should("be.checked"); we can use this method also by choosing the checkbox using index method
+
+
+
+    //     })
+    //   })
